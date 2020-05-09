3bb2bd7a64e1c9dfb61818c48eb6618a03761ddc
cicomsa <ioanacomsa85@gmail.com>
2020-05-09 17:16:13 +0100

    make ingredients amendments

--- src/components/pages/static/MenuItems/Item/Index.js
+++ src/components/pages/static/MenuItems/Item/Index.js
@@ -1,26 +1,78 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ItemsContext } from '../../../index'
 import styles from './styles.scss'
 
-const Index = ({ item }) => {
+const Index = ({ item, action, state }) => {
   const { setOrder, order } = useContext(ItemsContext)
+  const [amendment, setAmendment] = useState(false)
+  const alreadyChosen = state.find(el => el.id === item.id)
 
-  const handleClick = (e, value) => {
+  const addToOrder = (value) => {
     const items = order.includes(value)
       ? order.filter(id => id !== value)
       : [...order, value]
     setOrder(items)
   }
 
+  const amend = () => setAmendment(true)
+  const completeAmendment = () => setAmendment(false)
+
+  const setChoice = (id, value, choice) => {
+    action({
+      type: 'SET_PREFERENCE', payload: {
+        id,
+        [choice]: [value]
+      }
+    })
+  }
+
+  const removePreference = (value) => {
+    const choice = preference.extra.includes(value) ? 'extra' : 'without'
+    const choiced = {
+      [choice]: preference[choice].filter(ingredient => ingredient !== value)
+    }
+
+    setPreference({ ...preference, ...choiced })
+  }
+
   return (
     <div>
       <div className="item">
-        <h3 className="item__name" onClick={e => handleClick(e, item.id)}>
+        <h3 className="item__name">
           {item.name}
         </h3>
         <h3>£{item.price}</h3>
       </div>
       <em>{item.description}</em>
+      {amendment && <p>Only the following ingredients can be amended: </p>}
+      {amendment && (item.ingredients && item.ingredients.split(', ').map(ingredient => {
+        return <div key={ingredient}>
+          {(!alreadyChosen ||
+            (alreadyChosen && !alreadyChosen.extra) ||
+            (alreadyChosen && alreadyChosen.extra && !alreadyChosen.extra.includes(ingredient))) &&
+            <button onClick={() => setChoice(item.id, ingredient, 'extra')} type="button">+</button>
+          }
+          <em>{ingredient}</em>
+          {(!alreadyChosen ||
+            (alreadyChosen && !alreadyChosen.without) ||
+            (alreadyChosen && alreadyChosen.without && !alreadyChosen.without.includes(ingredient))) &&
+            <button onClick={() => setChoice(item.id, ingredient, 'without')} type="button">-</button>
+          }
+          {alreadyChosen && ((alreadyChosen.without && alreadyChosen.without.includes(ingredient)) || (alreadyChosen.extra && alreadyChosen.extra.includes(ingredient))) && (
+            <button onClick={() => removePreference(ingredient)} type="button">Remove preference option</button>
+          )}
+        </div>
+      }
+
+      )
+      )}
+      <div>
+        {!amendment && item.ingredients && <button onClick={amend}>Amend ingredients</button>}
+        {amendment && <button onClick={completeAmendment}>Complete amendment</button>}
+      </div>
+      <div>
+        {!amendment && <button onClick={() => addToOrder(item.id, preference)}>Add to order</button>}
+      </div>
       <style jsx>{styles}</style>
     </div>
   )
--- src/components/pages/static/MenuItems/index.js
+++ src/components/pages/static/MenuItems/index.js
@@ -1,16 +1,55 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext, useReducer } from 'react'
 import Item from './Item'
 import { ItemsContext } from '../../index'
 
+const initialState = []
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case 'SET_PREFERENCE':
+      const alreadyChosen = state.find(item => item.id === payload.id)
+
+      if (alreadyChosen) {
+        const items = state.map(item => {
+          if (item.id === payload.id) {
+            const payloadKey = Object.keys(payload)[1]
+            if (Object.keys(item).includes(payloadKey)) {
+              return {
+                ...item,
+                [payloadKey]: [...item[payloadKey], ...payload[payloadKey]]
+              }
+            }
+
+            return { ...item, [payloadKey]: payload[payloadKey] }
+          }
+          return item
+        })
+
+        return items
+
+      } else {
+        return [...state, payload]
+      }
+
+    default:
+      return initialState
+  }
+}
+
 const Index = () => {
   const { items, category } = useContext(ItemsContext)
+  const [state, action] = useReducer(reducer, initialState)
+
 
   const menuItems = category === 'all'
     ? items : items.filter(item => item.category === category)
   return (
     <div>
       {menuItems
-        .map(item => <Item key={item.id} item={item} />)
+        .map(item =>
+          <Item key={item.id} item={item} action={action} state={state} />
+
+        )
       }
     </div>
   )
